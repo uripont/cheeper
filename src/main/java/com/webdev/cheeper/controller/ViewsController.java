0d76e8143229f5130e514ed8ec8d9a757982bfaf
uripont-upf <oriol.pont01@estudiant.upf.edu>
2025-06-13 15:40:08 +0000

    creating separate endpoint for profile view obtention, migrating away from unified one

--- src/main/java/com/webdev/cheeper/controller/ViewsController.java
+++ src/main/java/com/webdev/cheeper/controller/ViewsController.java
@@ -8,41 +8,38 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/views/*")
+@WebServlet({
+    "/views/feed",
+    "/views/timeline",
+    "/views/users",
+    "/views/create",
+    "/views/post",
+    "/views/chats"
+})
 public class ViewsController extends HttpServlet {
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String pathInfo = req.getPathInfo();
-        if (pathInfo == null) {
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-            return;
-        }
-
-        // Remove leading slash
-        String view = pathInfo.substring(1);
+        String pathInfo = req.getServletPath();
         String jspPath;
 
-        switch (view) {
-            case "feed":
+        switch (pathInfo) {
+            case "/views/feed":
                 jspPath = "/WEB-INF/views/components/feed-view.jsp";
                 break;
-            case "timeline":
+            case "/views/timeline":
                 jspPath = "/WEB-INF/views/components/timeline-view.jsp";
                 break;
-            case "users":
+            case "/views/users":
                 jspPath = "/WEB-INF/views/components/users-list-view.jsp";
                 break;
-            case "create":
+            case "/views/create":
                 jspPath = "/WEB-INF/views/components/create-post-view.jsp";
                 break;
-            case "post":
+            case "/views/post":
                 jspPath = "/WEB-INF/views/components/post-view.jsp";
                 break;
-            case "profile":
-                jspPath = "/WEB-INF/views/components/profile-view.jsp";
-                break;
-            case "chats":
+            case "/views/chats":
                 jspPath = "/WEB-INF/views/components/chats-view.jsp";
                 break;
             default:
